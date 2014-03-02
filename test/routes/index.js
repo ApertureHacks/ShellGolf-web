46df3abb73111ff163fab7fb940ee45dae9da26f
Josh Matthews <josh@jmatthews.us>
2014-03-02 03:00:11 -0500

    Adding test for index route.

--- test/routes/index.js
+++ test/routes/index.js
@@ -1,7 +1,14 @@
+/* jshint -W030 */
 var db = require('../../lib/db')
+  , sinon = require('sinon')
+  , routes = require('../../routes')
   , challenge = require('./challenge');
 
 describe('routes', function() {
+
+  var req = {}
+    , res = { render: sinon.spy() };
+
   before(function(done) {
     db.connect('mongodb://localhost/shellgolf_test', function() {
       done();
@@ -13,6 +20,23 @@ describe('routes', function() {
     db.disconnect(function() {
       done();
     });
+  });
+
+  it('should render the home page', function(done) {
+    sinon.stub(db, 'Challenge');
+    db.Challenge.find = db.Challenge.limit = function() {
+      return this;
+    };
+    db.Challenge.exec = function(cb) {
+      cb(null, []);
+      res.render.calledOnce.should.be.true;
+      res.render.firstCall.args[0].should.equal('index');
+
+      db.Challenge.restore();
+      done();
+    };
+
+    routes.index(req, res);
   });
 
   challenge();
